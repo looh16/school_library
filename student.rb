1ff9155653e91de736a266106964e01dc4d520a6
looh16 <langacustodio@gmail.com>
2022-10-10 13:17:56 +0200

    Fix Linters errors

--- student.rb
+++ student.rb
@@ -1,16 +1,14 @@
-class Student > Person
-
+class Student < Person
   attr_reader :id
   attr_accessor :classroom
-  
+
   def initialize(classroom)
     super()
     @id = Random.rand(1..1000)
     @classroom = classroom
   end
-  
-  def play_hooky
-    return "¯\(ツ)/¯"
-  end
 
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
 end
--- teacher.rb
+++ teacher.rb
@@ -1,16 +1,14 @@
-class Teacher > Person
-
+class Teacher < Person
   attr_reader :id
   attr_accessor :specialization
-  
+
   def initialize(specialization)
     super()
     @id = Random.rand(1..1000)
     @specialization = specialization
   end
-  
-  def can_use_services?
-    return true
-  end
 
+  def can_use_services?
+    true
+  end
 end
