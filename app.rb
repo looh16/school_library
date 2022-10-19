414ef5d6303416978ab84a0eccd6ca82ac1ef380
Pierre <piercel.coder2022@gmail.com>
2022-10-19 10:22:46 +0200

    Add the workflow

--- app.rb
+++ app.rb
@@ -10,21 +10,6 @@ class App
     @output = 0
   end
 
-  def options
-    print 'Welcome to School Library App!'
-    while @output != 7
-      print "\nPlease choose an option by entering a number: \n\n"
-      print "1 - List all books\n"
-      print "2 - List all people\n"
-      print "3 - Create a person\n"
-      print "4 - Create a book\n"
-      print "5 - Create a rental\n"
-      print "6 - List all rentals for a given person id\n"
-      print "7 - Exit\n"
-      start_app
-    end
-  end
-
   def start_app
     @output = gets.chomp.to_i
     case @output
--- main.rb
+++ main.rb
@@ -2,8 +2,23 @@ require './app'
 
 class Main
   def run
-    app = App.new
-    app.options
+    @app = App.new
+    options
+  end
+
+  def options
+    print 'Welcome to School Library App!'
+    while @output != 7
+      print "\nPlease choose an option by entering a number: \n\n"
+      print "1 - List all books\n"
+      print "2 - List all people\n"
+      print "3 - Create a person\n"
+      print "4 - Create a book\n"
+      print "5 - Create a rental\n"
+      print "6 - List all rentals for a given person id\n"
+      print "7 - Exit\n"
+      @app.start_app
+    end
   end
 end
 
