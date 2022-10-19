0edd00e7a56857ad07e9c05e4d82f6f56caae1ad
looh16 <langacustodio@gmail.com>
2022-10-19 16:16:45 +0200

    Update create_book file

--- create_book.rb
+++ create_book.rb
@@ -1,9 +1,12 @@
 require './book'
 require './create_rental'
+require 'json'
 
 class CreateBook
   def initialize
-    @books = []
+    file = File.open("books.json")
+    @books = File.read("books.json").split
+    file.close
   end
 
   def create_new_book
@@ -13,14 +16,14 @@ class CreateBook
     author = gets.chomp
     book = Book.new(title, author)
     @books.push(book)
-
+    File.open("books.json", "w") { |f| f.write(@books)}
     p "Book #{title} has been added successfully!!!"
   end
 
   def list_books
     if @books.length.positive?
       @books.each_with_index do |book, i|
-        p " book number: #{i}, ID: #{book.id}, Title: #{book.title}, Author: #{book.author}"
+        p ("#{@id}")
       end
     else
       p 'books not found'
