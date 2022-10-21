d9691dfd4fd94dc65b47c665ac7f013258723e86
Pierre <piercel.coder2022@gmail.com>
2022-10-21 10:51:41 +0200

    Add the student specification

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -3,8 +3,8 @@ require './student'
 describe Student do
   context 'When testing the Student class' do
     it 'Should Create a Student' do
-      student = Student.new('2', 95, 'Jhon')
-      expect(student.name).to eq('Jhon')
+      student = Student.new('2', 95, 'John')
+      expect(student.name).to eq('John')
     end
   end
 end
